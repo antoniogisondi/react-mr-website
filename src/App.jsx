251bfc8c0605c3fda7fab9c3573792de56b7f26c
Antonio Gisondi <anto.gisondi@tiscali.it>
2024-11-07 18:23:40 +0100

    initialize repository

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
-import {  } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
+import HomePage from './pages/HomePage/HomePage'
+import Services from './pages/Services/Services'
+import Projects from './pages/Projects/Projects'
+import Contacts from './pages/Contacts/Contacts'
+import About from './pages/About/About'
 
 function App() {
   return (
-    <div>App</div>
+    <>
+      <Router>
+        <Routes>
+          <Route path='/' element={<HomePage/>}/>
+          <Route path='/servizi' element={<Services/>}/>
+          <Route path='/progetti' element={<Projects/>}/>
+          <Route path='/contatti' element={<Contacts/>}/>
+          <Route path='/chi-siamo' element={<About/>}/>
+        </Routes>
+      </Router>
+    </>
   )
 }
 
